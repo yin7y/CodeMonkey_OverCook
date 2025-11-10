c8caef39c33f9e1f5f00cf15a64e88a23a007a3e
7uuuuu <heibaiyin7@gmail.com>
2025-11-10 11:15:31 +0800

    p25 Completed & p24 bug fixed
    kitchenObject on plate is !validKitchenObjectSOList.Contains(kitchenObjectSO)

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs{
+        public KitchenObjectSO kitchenObjectSO;
+    }
+    
     [SerializeField] List<KitchenObjectSO> validKitchenObjectSOList;
     
     List<KitchenObjectSO> kitchenObjectSOList;
@@ -14,13 +20,17 @@ public class PlateKitchenObject : KitchenObject
     }
     
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO){
-        if(validKitchenObjectSOList.Contains(kitchenObjectSO)){
+        if(!validKitchenObjectSOList.Contains(kitchenObjectSO)){
             return false;
         }
         if(kitchenObjectSOList.Contains(kitchenObjectSO)){
             return false;
         }else{
             kitchenObjectSOList.Add(kitchenObjectSO);
+            
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs{
+                kitchenObjectSO = kitchenObjectSO,
+            });
             return true;
         }
         
